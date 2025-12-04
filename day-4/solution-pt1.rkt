d1c98d3487f2d9998bca8b3447afbee014c648ff
Lukas Rhoades <lukasrhoades11@gmail.com>
2025-12-04 12:16:30 +0000

    add day 4

---
+++ day-4/solution-pt1.rkt
@@ -0,0 +1,70 @@
+#!/usr/bin/env racket
+#lang racket/base
+
+(require racket/port)
+
+(define in (open-input-file "input.txt"))
+
+(define (grid-search prev curr next res)
+  (let ((next (read-line in)))
+    (if (eof-object? next)
+        (+ res (check-adjacent 0 prev curr last-line 0))
+        (let ((next-list (regexp-match* #px"\\S{1}" next)))
+          (grid-search curr
+                       next-list
+                       0
+                       (+ res (check-adjacent
+                              0
+                              prev
+                              curr
+                              next-list
+                              0)))))))
+
+(define (check-adjacent i prev curr-row next sum)
+  (let ((len (length curr-row)))
+    (if (= i len)
+        sum
+        (let ((curr-pos (index i curr-row)))
+          (cond ((equal? curr-pos ".")
+                 (check-adjacent (+ i 1) prev curr-row next sum))
+                (else
+                  (let ((tl (index (- i 1) prev))
+                        (tt (index i prev))
+                        (tr (index (+ i 1) prev))
+                        (ml (index (- i 1) curr-row))
+                        (mr (index (+ i 1) curr-row))
+                        (bl (index (- i 1) next))
+                        (bb (index i next))
+                        (br (index (+ i 1) next)))
+                    (let ((test (paper-test
+                                  (list tl tt tr ml mr bl bb br)
+                                  0)))
+                      (check-adjacent (+ i 1)
+                                      prev
+                                      curr-row
+                                      next
+                                      (+ sum test))))))))))
+
+(define (paper-test positions count)
+  (cond ((> count 3) 0)
+        ((null? positions) 1)
+        ((equal? (car positions) "@")
+         (paper-test (cdr positions) (+ count 1)))
+        ((equal? (car positions) ".")
+         (paper-test (cdr positions) count))))
+
+(define (index i list)
+  (cond ((or (< i 0) (= i (length list))) ".")
+        ((= i 0) (car list))
+        (else (index (- i 1) (cdr list)))))
+
+(define first-line (regexp-match* #px"\\S{1}" (read-line in)))
+(define len (length first-line))
+(define (builder len char)
+  (if (= len 0)
+      '()
+      (cons char (builder (- len 1) char))))
+(define prev-line (builder len "."))
+(define last-line prev-line)
+
+(grid-search prev-line first-line 0 0)
---
+++ day-4/solution-pt2.rkt
@@ -0,0 +1,130 @@
+#!/usr/bin/env racket
+#lang racket/base
+
+(require racket/port)
+
+(define in (open-input-file "input.txt"))
+
+(define (multi-grid grid total-sum)
+  (let ((new-gs (grid-search #f
+                             prev-line
+                             (regexp-match* #px"\\S{1}"
+                                            (car grid))
+                             0
+                             0
+                             (cdr grid)
+                             '())))
+    (let ((new-sum (car new-gs)) (new-grid (cdr new-gs)))
+      (if (= new-sum 0)
+          total-sum
+          (multi-grid new-grid (+ total-sum new-sum))))))
+
+(define (grid-search first? prev curr next res curr-grid new-grid)
+  (if first?
+      (let ((next (read-line in)))
+        (if (eof-object? next)
+            (let ((last-row-check
+                    (check-adjacent 0 prev curr last-line "" 0)))
+              (cons (+ res (car last-row-check))
+                    (append new-grid (list (cdr last-row-check)))))
+            (let ((next-list (regexp-match* #px"\\S{1}" next)))
+              (let ((row-check (check-adjacent
+                                 0
+                                 prev
+                                 curr
+                                 next-list
+                                 ""
+                                 0)))
+                (grid-search #t
+                             curr
+                             next-list
+                             0
+                             (+ res (car row-check))
+                             curr-grid
+                             (append new-grid (list (cdr row-check))))))))
+      (if (null? curr-grid)
+          (let ((last-row-check
+                  (check-adjacent 0 prev curr last-line "" 0)))
+            (cons (+ res (car last-row-check))
+                  (append new-grid (list (cdr last-row-check)))))
+          (let ((next (car curr-grid)))
+            (let ((next-list (regexp-match* #px"\\S{1}" next)))
+              (let ((row-check (check-adjacent
+                                 0
+                                 prev
+                                 curr
+                                 next-list
+                                 ""
+                                 0)))
+                (grid-search #f
+                             curr
+                             next-list
+                             0
+                             (+ res (car row-check))
+                             (cdr curr-grid)
+                             (append new-grid (list (cdr row-check))))))))))
+
+(define (check-adjacent i prev curr-row next new-row sum)
+  (let ((len (length curr-row)))
+    (cond ((= i len) (cons sum new-row))
+          (else (let ((curr-pos (index i curr-row)))
+                  (cond ((equal? curr-pos ".")
+                         (check-adjacent (+ i 1)
+                                         prev
+                                         curr-row
+                                         next
+                                         (string-append
+                                           new-row
+                                           curr-pos)
+                                         sum))
+                        (else (let ((tl (index (- i 1) prev))
+                                    (tt (index i prev))
+                                    (tr (index (+ i 1) prev))
+                                    (ml (index (- i 1) curr-row))
+                                    (mr (index (+ i 1) curr-row))
+                                    (bl (index (- i 1) next))
+                                    (bb (index i next))
+                                    (br (index (+ i 1) next)))
+                                (let ((test (paper-test
+                                              (list tl tt tr ml mr bl bb br)
+                                              0)))
+                                  (check-adjacent (+ i 1)
+                                                  prev
+                                                  curr-row
+                                                  next
+                                                  (string-append
+                                                    new-row
+                                                    (which-char
+                                                      test
+                                                      curr-pos))
+                                                  (+ sum test)))))))))))
+
+(define (which-char test char)
+  (if (= test 1)
+      "."
+      char))
+
+(define (paper-test positions count)
+  (cond ((> count 3) 0)
+        ((null? positions) 1)
+        ((equal? (car positions) "@")
+         (paper-test (cdr positions) (+ count 1)))
+        ((equal? (car positions) ".")
+         (paper-test (cdr positions) count))))
+
+(define (index i list)
+  (cond ((or (< i 0) (= i (length list))) ".")
+        ((= i 0) (car list))
+        (else (index (- i 1) (cdr list)))))
+
+(define first-line (regexp-match* #px"\\S{1}" (read-line in)))
+(define len (length first-line))
+(define (builder len char)
+  (if (= len 0)
+      '()
+      (cons char (builder (- len 1) char))))
+(define prev-line (builder len "."))
+(define last-line prev-line)
+
+(define initial (grid-search #t prev-line first-line 0 0 0 '()))
+(multi-grid (cdr initial) (car initial))
